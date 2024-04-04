5f3c4eb1692784b600ad0796051c9ffeddfbbc75
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-04 23:31:28 +0300

    create navigation component and include it in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation />
-      <Logo />
+      <Navigation />
+      {/* <Logo />
       <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
