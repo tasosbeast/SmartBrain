879dc495d64304a563630388d938445e8becd99f
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-05 00:20:53 +0300

    Add react-parallax-tilt library to package.json and package-lock.json

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <Tilt
+      className="background-stripes parallax-effect-glare-scale"
+      perspective={500}
+      glareEnable={true}
+      glareMaxOpacity={0.45}
+      scale={1.02}
+    >
+      <div className="inner-element">
+        <div>React</div>
+        <div>Parallax Tilt</div>
+        <div>👀</div>
+      </div>
+    </Tilt>
+  );
+};
+
+export default Logo;
