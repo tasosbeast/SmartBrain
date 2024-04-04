b653d44d4fe125f9bf692c5e178c8e82bacef836
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-05 00:57:14 +0300

    Update Logo component styles and add logo image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
 import "./Logo.css";
+import logoimg from "./Logo.png";
 
 const Logo = () => {
   return (
     <Tilt
-      className="background-stripes parallax-effect-glare-scale"
+      className="background-stripes parallax-effect-glare-scale ma4 mt0"
       perspective={500}
       glareEnable={true}
       glareMaxOpacity={0.45}
       scale={1.02}
     >
       <div className="inner-element">
-        <div>React</div>
-        <div>Parallax Tilt</div>
-        <div>👀</div>
+        <div className="pa4">
+          <img src={logoimg} alt="logo" />
+        </div>
       </div>
     </Tilt>
   );
