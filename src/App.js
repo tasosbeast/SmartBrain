2d41d57553e281f49500daa0f9d22e0f127527b2
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-05 22:12:11 +0300

    Refactor App.js to log the bounding box of the response in the console

--- src/App.js
+++ src/App.js
@@ -83,7 +83,9 @@ function App() {
       .then((response) => response.json())
       .then(
         function (response) {
-          console.log(response);
+          console.log(
+            response.outputs[0].data.regions[0].region_info.bounding_box
+          );
         },
         function (err) {
           // there was an error
