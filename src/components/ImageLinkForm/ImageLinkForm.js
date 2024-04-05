302235f2f04d7534e89009ec77e784665b33c3c3
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-05 18:56:20 +0300

    Include Rank component in App.js and update ImageLinkForm component styles

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import "./ImageLinkForm.css";
 const ImageLinkForm = () => {
   return (
     <div>
-      <p className="f3">
+      <p className="f3 white">
         {"This Magic Brain will detect faces in your pictures. Give it a try!"}
       </p>
       <div className="center">
