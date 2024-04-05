bb46e3a620b38257e98a1f8f59c1bd0952a0c69b
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2024-04-05 20:46:17 +0300

    Refactor ImageLinkForm component to handle input change and button submit events

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -6,13 +7,27 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import ParticlesBg from "particles-bg";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log("click");
+  };
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/* <FaceRecognition /> */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3 white">
@@ -9,8 +9,15 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="washed-blue b pv2 ph3 bg-blue hover-bg-light-blue bn br2 hover-shadow-inner w-30 grow">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="washed-blue b pv2 ph3 bg-blue hover-bg-light-blue bn br2 hover-shadow-inner w-30 grow"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
